32b991897a1247423085d442d04ee53fbf43f576
marie-emilienne-gnamien <marie-emilienne.gnamien@edu.esiee.fr>
2026-01-05 11:34:03 +0000

    suppresion du jour et mois dans la colonne date

--- main.py
+++ main.py
@@ -5,10 +5,19 @@ import pandas as pd
 
 filename = "synthetic_disaster_events_2025.csv"
 
-# data = pd.read_csv(filename)
-# data = data["City"].unique()
+data = pd.read_csv(filename)
 
-# print(data)
+# dates = data['date']
+# dates = dates.astype('str')
+
+l = []
+for i in range(len(data['date'])):
+    l.append((int(str(data['date'][i][0:4])))) #modification de la colonne date, afin de juste garder les années
+data['date'] = l
+
+
+print(data['date'])
+# print(dates.dtype)
 # # l = []
 # # for la, lon in zip(data['lat'], data['lng']):
 # #     l.append((la, lon))
